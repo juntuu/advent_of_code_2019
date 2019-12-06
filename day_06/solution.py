85fb7f803e695a8cfe2c100d12b906b11778356d
Juho Eerola <juho.eerola@tuni.fi>
2019-12-06 11:47:58 +0200

    Day 6, part 2

--- day_06/solution.py
+++ day_06/solution.py
@@ -24,3 +24,22 @@ def count_orbits(*names, counts=None):
 total_orbits = sum(x for _, x in count_orbits(*orbits))
 print('Day 5, part 1:', total_orbits)
 
+
+def transfers_between(a, b):
+	trail_a = []
+	while a in orbits:
+		a = orbits[a]
+		trail_a.append(a)
+
+	a_set = set(trail_a)
+	trail_b = []
+	while b not in a_set:
+		b = orbits[b]
+		trail_b.append(b)
+
+	return trail_a.index(b) + len(trail_b) - 1
+
+
+transfers = transfers_between('YOU', 'SAN')
+print('Day 5, part 2:', transfers)
+
