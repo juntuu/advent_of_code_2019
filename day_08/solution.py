da5c653fc928b47576e4c2bf9085e2f86e345eba
Juho Eerola <juho.eerola@tuni.fi>
2019-12-08 19:13:49 +0200

    Day 8, part 2

--- day_08/solution.py
+++ day_08/solution.py
@@ -12,3 +12,12 @@ least_zeros = min(counts, key=lambda c: c['0'])
 
 print('Day 8, part 1:', least_zeros['1'] * least_zeros['2'])
 
+print('Day 8, part 2:')
+
+layers = zip(*image)
+for _ in range(HEIGHT):
+	for i in range(WIDTH):
+		visible = ('#' if c == '1' else ' ' for c in next(layers) if c != '2')
+		print(next(visible, ' ') * 2, end='')
+	print()
+
