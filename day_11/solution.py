72c02d2b8d0d2592eb7ccbccf7cfc3ce37ba85f6
Juho Eerola <juho.eerola@tuni.fi>
2019-12-11 10:29:50 +0200

    Day 11, part 2

--- day_11/solution.py
+++ day_11/solution.py
@@ -6,14 +6,12 @@ with open('input.txt') as f:
 	prog = [int(x) for x in f.read().strip().split(',')]
 
 
-async def bot(prog):
+async def bot(prog, white, *, x=0, y=0):
 	io = asyncio.Queue(), asyncio.Queue()
 	cpu = Intcode(*io)
 	task = asyncio.Task(cpu.task(prog))
 
 	painted = set()
-	white = set()
-	x, y = 0, 0
 	turns = [0, 1, 0, -1, 0]
 	direction = 0
 	while not task.done():
@@ -34,6 +32,23 @@ async def bot(prog):
 	await task
 	return len(painted)
 
-painted = asyncio.run(bot(prog))
+
+def print_msg(white, bw=(' ', '#')):
+	x_range = range(min(x for x, _ in white), max(x for x, _ in white) + 1)
+	y_range = range(max(y for _, y in white), min(y for _, y in white) - 1, -1)
+	for y in y_range:
+		line = []
+		for x in x_range:
+			line.append(bw[(x, y) in white] * 2)
+		print(''.join(line))
+
+
+painted = asyncio.run(bot(prog, set()))
 print('Day 11, part 1:', painted)
+
+start_x = start_y = 0
+white = {(start_x, start_y)}
+asyncio.run(bot(prog, white, x=start_x, y=start_y))
+print('Day 11, part 2:')
+print_msg(white)
 
