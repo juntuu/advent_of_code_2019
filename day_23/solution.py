714f00811aea87f0b0d969d35b401bf58a5260ba
Juho Eerola <juho.eerola@tuni.fi>
2019-12-23 17:42:54 +0200

    Day 23, part 2

--- day_23/solution.py
+++ day_23/solution.py
@@ -4,13 +4,40 @@ import asyncio
 from intcode import Intcode
 
 
-class NIC:
-	cond = None
-	first = None
+class NAT:
+	def __init__(self, qs):
+		self.qs = qs
+		self.packet = None
+		self.last = None
+		self.first = None
+		self.cond = asyncio.Condition()
 
-	def __init__(self, address, qs):
+	async def go(self):
+		while True:
+			async with self.cond:
+				await self.cond.wait_for(lambda: self.packet and not any(self.qs))
+				self.qs[0].extend(self.packet)
+				_, y = self.packet
+				if self.last == y:
+					return self.first, y
+				self.last = y
+				self.packet = None
+
+	async def ping(self):
+		async with self.cond:
+			self.cond.notify(1)
+
+	def send(self, x, y):
+		if self.first is None:
+			self.first = y
+		self.packet = x, y
+
+
+class NIC:
+	def __init__(self, address, qs, nat):
 		self.address = address
 		self.qs = qs
+		self.nat = nat
 		self.inq = []
 
 	async def get(self):
@@ -18,7 +45,10 @@ class NIC:
 		if not q:
 			await asyncio.sleep(0)
 			return -1
-		return q.pop(0)
+		val = q.pop(0)
+		if not q:
+			await self.nat.ping()
+		return val
 
 	async def put(self, x):
 		self.inq.append(x)
@@ -26,32 +56,30 @@ class NIC:
 			dest, x, y = self.inq
 			self.inq.clear()
 			if dest == 255:
-				NIC.first = y
-				async with NIC.cond:
-					NIC.cond.notify(1)
+				self.nat.send(x, y)
 			else:
 				self.qs[dest].extend([x, y])
 
 
 async def main(prog):
 	qs = [[i] for i in range(50)]
+	nat = NAT(qs)
 	ts = []
-	NIC.cond = asyncio.Condition()
 	for i, q in enumerate(qs):
-		switch = NIC(i, qs)
+		switch = NIC(i, qs, nat)
 		t = asyncio.Task(Intcode(switch, switch).task(prog[:]))
 		ts.append(t)
-	async with NIC.cond:
-		await NIC.cond.wait_for(lambda: NIC.first)
+	first, repeat = await nat.go()
 	for t in ts:
 		t.cancel()
-	return NIC.first
+	return first, repeat
 
 
 name = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
 with open(name) as f:
 	prog = [int(x) for x in f.read().strip().split(',')]
 
-first = asyncio.run(main(prog))
+first, repeat = asyncio.run(main(prog))
 print('Day 23, part 1:', first)
+print('Day 23, part 2:', repeat)
 
